74f515dc7222c4552632d457dbd03cfd510f0598
Oksana Oberemok <Pylypenkok@gmail.com>
2023-07-31 20:51:31 +0700

    add general functions  to set date in notes

---
+++ src/lib.ts
@@ -0,0 +1,31 @@
+const MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const DATE_REGEX =
+  /(?:^|\D)(?:(?:0?[1-9]|[12][0-9]|3[01])\/(?:0?[1-9]|1[0-2])\/(?:\d{4}))(?:\D|$)/gm;
+
+export const setDate = () => {
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  return `${MONTHS[month]} ${day}, ${year}`;
+};
+
+export const checkForDates = (date: string) => {
+  const match = date.match(DATE_REGEX);
+
+  return match?.join(", ") || "";
+};
