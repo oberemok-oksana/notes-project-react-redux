977b091c1d4324ae438436d93de3e4ce27867246
Oksana Oberemok <Pylypenkok@gmail.com>
2023-07-31 19:37:58 +0700

    change Button component by adding text anf func through props for reuse

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import "./App.css";
+import { useState } from "react";
 import Button from "./components/Button";
+import Form from "./components/Form";
 import Table from "./components/Table";
 
 function App() {
+  const [formVisibility, setFormVisibility] = useState(false);
   return (
     <>
       <Table />
       <div className="button-wrapper">
-        <Button />
+        <Button
+          text="Create note"
+          onClick={() => setFormVisibility((prev) => !prev)}
+        />
       </div>
+      {formVisibility && <Form />}
     </>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,14 @@
-const Button = () => {
-  return <button className="button">Create note</button>;
+type ButtonPropsType = {
+  text: string;
+  onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+};
+
+const Button = ({ text, onClick }: ButtonPropsType) => {
+  return (
+    <button className="button" onClick={onClick}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
