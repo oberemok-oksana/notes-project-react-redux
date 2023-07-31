07eb9c3af561dfedd9ddda86ae604d111cb91b5e
Oksana Oberemok <Pylypenkok@gmail.com>
2023-07-31 20:53:35 +0700

    implement from hide after sending data, implement adding data to the store

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,33 +1,62 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { add } from "../features/notes/notesSlice";
+import { hideCreatingForm } from "../features/ui/uiSlice";
+
+type Inputs = {
+  title: string;
+  content: string;
+  category: string;
+};
+
 const Form = () => {
+  const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    dispatch(add(data));
+    reset();
+    dispatch(hideCreatingForm());
+  };
+
   return (
-    <form className="card">
+    <form className="card" onSubmit={handleSubmit(onSubmit)}>
       <input
         className="input"
-        name="title"
         type="text"
         id="name"
         placeholder="Note title"
-        required
+        {...register("title", { required: true })}
       />
+      {errors.title && <span>This field is required</span>}
       <div className="wrapper">
         <label htmlFor="category">Choose a note category:</label>
-        <select className="select" name="category" id="category" required>
+        <select
+          className="select"
+          id="category"
+          {...register("category", { required: true })}
+        >
           <option value="">Choose something</option>
           <option value="Task">Task</option>
           <option value="Random Thought">Random Thought</option>
           <option value="Idea">Idea</option>
           <option value="Quote">Quote</option>
         </select>
+        {errors.category && <span>This field is required</span>}
       </div>
 
       <textarea
         className="input"
-        name="content"
         id="note-content"
         cols={30}
         rows={10}
-        required
+        {...register("content", { required: true })}
       ></textarea>
+      {errors.content && <span>This field is required</span>}
 
       <button type="submit" className="button" id="save-note-btn">
         Save
