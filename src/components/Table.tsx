d56a2ecc431a45d5aa95796fb00618f1c9b4cd21
Oksana Oberemok <Pylypenkok@gmail.com>
2023-07-31 18:39:42 +0700

    replace static data with NoteTr using redux store

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,4 +1,11 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
+import NoteTr from "./NoteTr";
+
 const Table = () => {
+  const notes = useSelector((state: RootState) => state.notes.value);
+  console.log(notes);
+
   return (
     <table className="table">
       <thead className="table-head">
@@ -19,48 +26,9 @@ const Table = () => {
         </tr>
       </thead>
       <tbody>
-        <tr className="table-data">
-          <td>
-            <div className="category">
-              <img src="/images/icons8-cart-30.png" alt="task" />
-            </div>
-          </td>
-          <td>Shopping list</td>
-          <td>April 20, 2021</td>
-          <td>Task</td>
-          <td>Tomatoes, bread</td>
-          <td></td>
-          <td className="table__td">
-            <img src="/images/icons8-edit-24.png" alt="editing" />
-          </td>
-          <td className="table__td">
-            <img src="/images/icons8-download-24.png" alt="archiving" />
-          </td>
-          <td className="table-head__td">
-            <img src="/images/icons8-delete-24.png" alt="deleting" />
-          </td>
-        </tr>
-        <tr className="table-data">
-          <td>
-            <div className="category">
-              <img src="/images/icons8-get-quote-30.png" alt="task" />
-            </div>
-          </td>
-          <td>William Gaddis</td>
-          <td>April 27, 2021</td>
-          <td>Quote</td>
-          <td>Power doesn't ...</td>
-          <td></td>
-          <td className="table__td">
-            <img src="/images/icons8-edit-24.png" alt="editing" />
-          </td>
-          <td className="table__td">
-            <img src="/images/icons8-download-24.png" alt="archiving" />
-          </td>
-          <td className="table-head__td">
-            <img src="/images/icons8-delete-24.png" alt="deleting" />
-          </td>
-        </tr>
+        {notes.map((note) => (
+          <NoteTr key={note.id} note={note} />
+        ))}
       </tbody>
     </table>
   );
