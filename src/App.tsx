f49d5ace8e8f28d9aeb893e991e623e35fca0323
Oksana Oberemok <Pylypenkok@gmail.com>
2023-07-31 20:54:01 +0700

    add toggle for hiding form

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
-import { useState } from "react";
 import Button from "./components/Button";
 import Form from "./components/Form";
 import Table from "./components/Table";
+import { toggle } from "./features/ui/uiSlice";
+import { RootState } from "./store";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
-  const [formVisibility, setFormVisibility] = useState(false);
+  const creatingFormIsVisible = useSelector(
+    (state: RootState) => state.ui.creating
+  );
+  const dispatch = useDispatch();
+
   return (
     <>
       <Table />
       <div className="button-wrapper">
         <Button
+          type="button"
           text="Create note"
-          onClick={() => setFormVisibility((prev) => !prev)}
+          onClick={() => dispatch(toggle())}
         />
       </div>
-      {formVisibility && <Form />}
+      {creatingFormIsVisible && <Form />}
     </>
   );
 }
