54abfe430d97aa5033892fd2ff59e26df9bae494
Oksana Oberemok <Pylypenkok@gmail.com>
2023-07-31 18:38:53 +0700

    create NoteTr component

---
+++ src/components/NoteTr.tsx
@@ -0,0 +1,62 @@
+import { NoteType } from "../types";
+
+type NoteTrPropsType = {
+  note: NoteType;
+};
+
+const NoteTr = ({ note }: NoteTrPropsType) => {
+  let categoryImageSrc = "/images/icons8-cart-30.png";
+  let categoryImageAlt = "something";
+
+  switch (note.category) {
+    case "Task":
+      categoryImageSrc = "./public/images/icons8-cart-30.png";
+      categoryImageAlt = "task";
+      break;
+
+    case "Random Thought":
+      categoryImageSrc = "./public/images/icons8-mind-30.png";
+      categoryImageAlt = "random thought";
+      break;
+
+    case "Idea":
+      categoryImageSrc = "./public/images/icons8-light-on-30.png";
+      categoryImageAlt = "idea";
+      break;
+
+    case "Quote":
+      categoryImageSrc = "./public/images/icons8-get-quote-30.png";
+      categoryImageAlt = "quote";
+      break;
+    default:
+      categoryImageSrc = "./public/images/icons8-cart-30.png";
+      categoryImageAlt = "quote";
+      break;
+  }
+
+  return (
+    <tr className="table-data">
+      <td>
+        <div className="category">
+          <img src={categoryImageSrc} alt={categoryImageAlt} />
+        </div>
+      </td>
+      <td>{note.title}</td>
+      <td>{note.created}</td>
+      <td>{note.category}</td>
+      <td>{note.content}</td>
+      <td>{note.dates}</td>
+      <td className="table__td">
+        <img src="/images/icons8-edit-24.png" alt="editing" />
+      </td>
+      <td className="table__td">
+        <img src="/images/icons8-download-24.png" alt="archiving" />
+      </td>
+      <td className="table-head__td">
+        <img src="/images/icons8-delete-24.png" alt="deleting" />
+      </td>
+    </tr>
+  );
+};
+
+export default NoteTr;
